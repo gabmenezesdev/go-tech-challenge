f53a9a3b870aca3fc10176813fcbba9c07824fb5
Gabriel dos Santos <gabrielsilva.mene@gmail.com>
2024-12-12 01:16:15 -0300

    feat: delete farm new test case

--- internal/application/use-case/delete_farm_test.go
+++ internal/application/use-case/delete_farm_test.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"testing"
 
 	farm "github.com/gabmenezesdev/go-tech-challenge/internal/domain/farm"
@@ -28,5 +29,27 @@ func TestDeleteFarm(t *testing.T) {
 	err = DeleteFarmUseCase.Execute(createdFarmId)
 	if err != nil {
 		t.Error("Error on deleting farm")
+	}
+}
+
+func TestDeleteFarm_NotFound(t *testing.T) {
+	t.Setenv("MONGODB_URL", "mongodb://localhost:27017")
+	t.Setenv("MONGODB_DATABASE", "teste")
+
+	farmMongoDbAdapter := repository.FarmRepositoryMongoAdapter{}
+
+	DeleteFarmUseCase, err := NewDeleteFarmUseCase(farmMongoDbAdapter)
+	if err != nil {
+		t.Fatalf("Failed to create DeleteFarmUseCase: %v", err)
+	}
+
+	nonexistentFarmId := "999999999999999999999999"
+
+	err = DeleteFarmUseCase.Execute(nonexistentFarmId)
+
+	if err == nil {
+		t.Errorf("Expected error for nonexistent farmId '%s', but got nil", nonexistentFarmId)
+	} else if err.Error() != fmt.Sprintf("farm not found with id: %s", nonexistentFarmId) {
+		t.Errorf("Unexpected error message: %v", err)
 	}
 }
