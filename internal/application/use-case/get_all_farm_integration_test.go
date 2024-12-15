09118ac4c06eb731d7732ab091c9864450f29eeb
Gabriel dos Santos <gabrielsilva.mene@gmail.com>
2024-12-15 14:18:36 -0300

    feat: add filter tests

--- internal/application/use-case/get_all_farm_integration_test.go
+++ internal/application/use-case/get_all_farm_integration_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gabmenezesdev/go-tech-challenge/internal/infra/repository"
 )
 
-func TestGetAllFarm(t *testing.T) {
+func TestGetAllFarmWithFilters(t *testing.T) {
 	t.Setenv("MONGODB_URL", "mongodb://localhost:27017")
 	t.Setenv("MONGODB_DATABASE", "teste")
 
@@ -20,17 +20,17 @@ func TestGetAllFarm(t *testing.T) {
 		t.Error(err)
 	}
 
-	firstFarm, err := farm.NewFarm("FirstFarm", 10.5, "hectare", "123 Farm Address")
+	firstFarm, err := farm.NewFarm("AppleFarm", 15.5, "hectare", "10 Orchard Lane")
 	if err != nil {
 		t.Error(err)
 	}
 
-	secondFarm, err := farm.NewFarm("SecondFarm", 20.7, "acre", "20 Address new")
+	secondFarm, err := farm.NewFarm("BerryFarm", 25.7, "acre", "25 Berry Street")
 	if err != nil {
 		t.Error(err)
 	}
 
-	thirdFarm, err := farm.NewFarm("ThirdFarm", 30.8, "square_km", "15 Faria Lima")
+	thirdFarm, err := farm.NewFarm("CherryFarm", 35.8, "square_km", "35 Cherry Drive")
 	if err != nil {
 		t.Error(err)
 	}
@@ -49,13 +49,65 @@ func TestGetAllFarm(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	farmFilter := dao.FarmFilters{}
-	foundFarms, err := NewGetAllFarmsUseCase.Execute(0, 2, farmFilter)
-	if err != nil {
-		t.Error(err)
+
+	tests := []struct {
+		name      string
+		filters   dao.FarmFilters
+		expectLen int
+	}{
+		{
+			name: "Filter by Name",
+			filters: dao.FarmFilters{
+				Name: "AppleFarm",
+			},
+			expectLen: 1,
+		},
+		{
+			name: "Filter by Unit",
+			filters: dao.FarmFilters{
+				Unit: "acre",
+			},
+			expectLen: 1,
+		},
+		{
+			name: "Filter by Land Area Less Than",
+			filters: dao.FarmFilters{
+				LandAreaLimit: "30",
+			},
+			expectLen: 2,
+		},
+		{
+			name: "Filter by Land Area Greater Than",
+			filters: dao.FarmFilters{
+				LandAreaInit: "20",
+			},
+			expectLen: 2,
+		},
+		{
+			name: "Filter by Land Area Range",
+			filters: dao.FarmFilters{
+				LandAreaInit:  "20",
+				LandAreaLimit: "40",
+			},
+			expectLen: 2,
+		},
+		{
+			name:      "No Filters",
+			filters:   dao.FarmFilters{},
+			expectLen: 3,
+		},
 	}
 
-	if len(foundFarms) == 0 {
-		t.Error("Expected to have an array of Farms, but got none")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			foundFarms, err := NewGetAllFarmsUseCase.Execute(0, 10, tt.filters)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(foundFarms) != tt.expectLen {
+				t.Errorf("Expected %d farms, but got %d", tt.expectLen, len(foundFarms))
+			}
+		})
 	}
 }
